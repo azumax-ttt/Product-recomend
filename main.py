4f9ccc2e96d8ffc920525149b1a5ca45e951f91a
AzumaTanimura <t.azumax0502@gmail.com>
2025-09-08 20:04:21 +0900

    商品表示作成

--- main.py
+++ main.py
@@ -5,7 +5,11 @@
 from dotenv import load_dotenv
 import logging
 import streamlit as st
+import utils
 from initialize import initialize
+import components as cn
+import constants as ct
+
 st.set_page_config(
     page_title=ct.APP_NAME
 )
@@ -28,3 +32,40 @@ if not "initialized" in st.session_state:
 cn.display_app_title()
 
 cn.display_initial_ai_message()
+
+try:
+    cn.display_conversation_log()
+except Exception as e:
+    logger.error(f"{ct.CONVERSATION_LOG_ERROR_MESSAGE}\n{e}")
+    st.error(utils.build_error_message(ct.CONVERSATION_LOG_ERROR_MESSAGE))
+    st.stop()
+
+chat_message = st.chat_input(ct.CHAT_INPUT_HELPER_TEXT)
+
+if chat_message:
+    logger.info({"message": chat_message})
+
+    with st.chat_message("user", avatar=ct.USER_ICON_FILE_PATH):
+        st.markdown(chat_message)
+
+    res_box = st.empty()
+    with st.spinner(ct.SPINNER_TEXT):
+        try:
+            result = st.session_state.retriever.invoke(chat_message)
+        except Exception as e:
+            logger.error(f"{ct.RECOMMEND_ERROR_MESSAGE}\n{e}")
+            st.error(utils.build_error_message(ct.RECOMMEND_ERROR_MESSAGE))
+            st.stop()
+    
+    with st.chat_message("assistant", avatar=ct.AI_ICON_FILE_PATH):
+        try:
+            cn.display_product(result)
+            
+            logger.info({"message": result})
+        except Exception as e:
+            logger.error(f"{ct.LLM_RESPONSE_DISP_ERROR_MESSAGE}\n{e}")
+            st.error(utils.build_error_message(ct.LLM_RESPONSE_DISP_ERROR_MESSAGE))
+            st.stop()
+
+    st.session_state.messages.append({"role": "user", "content": chat_message})
+    st.session_state.messages.append({"role": "assistant", "content": result})
